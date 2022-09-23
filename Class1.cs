57b93cd552d0bb96182db27439477842cb74052c
pranalikante123 <pranalikante21@gmail.com>
2022-09-23 13:48:12 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class HelloWorld
+{
+    static void Main()
+    {
+#if DebugConfig
+        Console.WriteLine("WE ARE IN THE DEBUG CONFIGURATION");
+#endif
+
+        Console.WriteLine("Hello, world!");
+    }
+}
